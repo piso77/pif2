7c72ee856a9c6c164b08e8ae19d36816a9c712ce
Paolo Pisati <paolo.pisati@canonical.com>
2016-12-22 16:50:15 +0000

    pifcli: import the remaining part of load()
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- software/src/pifcli.c
+++ software/src/pifcli.c
@@ -102,6 +102,69 @@ static void erase(pifHandle h) {
 	printf("erase done\n");
 }
 
+static void configureXO2(pifHandle h, FILE *fd) {
+  char *line = NULL, *eptr;
+  int i, num = 0;
+  const int CFG_PAGE_SIZE = 16;
+  size_t len = 0;
+  ssize_t read;
+  enum state {INITIAL, INDATA} machine = INITIAL;
+
+  printf("\n----------------------------\n");
+
+  pifWaitUntilNotBusy(h, -1);
+
+  pifDisableCfgInterface(h);
+  //showCfgStatus(h);
+  pifEnableCfgInterfaceOffline(h);
+
+  //showCfgStatus(h);
+  printf("erasing configuration memory..\n");
+  pifEraseCfg(h);
+  printf("erased..\n");
+
+  pifInitCfgAddr(h);
+  //showCfgStatus(h);
+  printf("programming configuration memory..\n"); // up to 2.2 secs in a -7000
+
+  while ((read = getline(&line, &len, fd)) != -1) {
+    uint8_t frameData[CFG_PAGE_SIZE];
+    uint8_t c;
+    char *tmp;
+    //printf("Retrieved line[%d] of length: %zu\n", num, read);
+    num++;
+    //printf("%s", line);
+    if (line[0] != '0' && line [0] != '1') {
+      if (machine == INITIAL)
+        continue;
+      if (machine == INDATA)
+        break;
+    }
+    machine = INDATA;
+    for (i=0; i < CFG_PAGE_SIZE; i++) {
+      tmp = strndup(&line[i*8], 8);
+      c = (uint8_t)strtol(tmp, &eptr, 2);
+      printf("tmp: %s hex: 0x%02x\n", tmp, c);
+      frameData[i] = c;
+      free(tmp);
+    }
+    pifProgCfgPage(h, frameData);
+    if ((num % 25)==0)
+      printf(".");
+  }
+  printf("\n");
+
+  //showCfgStatus(h);
+
+  printf("programmed. transferring..\n");
+  pifProgDone(h);
+  pifRefresh(h);
+
+  pifDisableCfgInterface(h);
+  //showCfgStatus(h);
+  printf("configuration done\n");
+}
+
 static void load(pifHandle h) {
 	FILE *fd;
 
@@ -114,7 +177,8 @@ static void load(pifHandle h) {
 	showDeviceID(h);
 	showTraceID(h);
 	//  showUsercode(h);
-	//configureXO2(h, fd);
+	configureXO2(h, fd);
+	fclose(fd);
 }
 
 struct opt opts[] = {
