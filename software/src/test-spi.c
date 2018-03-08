12f1db653dfdfa12837d24dad6801080b867c0fe
Paolo Pisati <paolo.pisati@canonical.com>
2018-03-08 16:00:36 +0000

    test-spi: align a bit more with machxo2-spi.c driver
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- software/src/test-spi.c
+++ software/src/test-spi.c
@@ -10,7 +10,74 @@
 #include <linux/spi/spidev.h>
 #include <sys/ioctl.h>
 
+/* MachXO2 Programming Guide - sysCONFIG Programming Commands */
+
+#define IDCODE_PUB              {0xe0, 0x00, 0x00, 0x00}
+#define ISC_ENABLE              {0xc6, 0x08, 0x00, 0x00}
+#define ISC_ERASE               {0x0e, 0x04, 0x00, 0x00}
+#define ISC_PROGRAMDONE         {0x5e, 0x00, 0x00, 0x00}
+#define LSC_INITADDRESS         {0x46, 0x00, 0x00, 0x00}
+#define LSC_PROGINCRNV          {0x70, 0x00, 0x00, 0x01}
+#define LSC_READ_STATUS         {0x3c, 0x00, 0x00, 0x00}
+#define LSC_REFRESH             {0x79, 0x00, 0x00, 0x00}
+
+/*
+ * Max CCLK in Slave SPI mode according to 'MachXO2 Family Data
+ * Sheet' sysCONFIG Port Timing Specifications (3-36)
+ */
+#define MACHXO2_MAX_SPEED       66000000
+
+#define MACHXO2_LOW_DELAY       5       /* us */
+#define MACHXO2_HIGH_DELAY      200     /* us */
+#define MACHXO2_REFRESH         4800    /* us */
+#define MACHXO2_MAX_BUSY_LOOP   32
+
+#define MACHXO2_PAGE_SIZE       16
+
+/* Status register bits and error codes */
+#ifdef __BIG_ENDIAN
+#define BUSY    20
+#define DONE    16
+#define DVER    3
+#define ENAB    17
+#define ERRBIT0 15
+#define ERRBIT1 0
+#define ERRBIT2 1
+#define FAIL    21
+#else
+#define BUSY    12
+#define DONE    8
+#define DVER    27
+#define ENAB    9
+#define ERRBIT0 23
+#define ERRBIT1 24
+#define ERRBIT2 25
+#define FAIL    13
+#endif
+#define ERRMASK 7
+
+#define ENOERR  0 /* no error */
+#define EID     1
+#define ECMD    2
+#define ECRC    3
+#define EPREAM  4 /* preamble error */
+#define EABRT   5 /* abort error */
+#define EOVERFL 6 /* overflow error */
+#define ESDMEOF 7 /* SDM EOF */
+
 #define RX_LEN 32
+
+#ifdef CONFIG_64BIT
+#define BITS_PER_LONG 64
+#else
+#define BITS_PER_LONG 32
+#endif /* CONFIG_64BIT */
+
+#define BIT_WORD(nr)            ((nr) / BITS_PER_LONG)
+static inline int test_bit(int nr, const volatile unsigned long *addr)
+{
+    return 1UL & (addr[BIT_WORD(nr)] >> (nr & (BITS_PER_LONG-1)));
+}
 
 //! Byte swap unsigned short
 uint16_t swap_uint16( uint16_t val ) 
@@ -55,31 +122,40 @@ int spi_xfer(int fd, char *reg, size_t sreg, void *buf, size_t sbuf) {
 	return ret;
 }
 
-void print_status_reg(unsigned int status) {
-	unsigned int errcode;
-	char *fcstatus;
+static inline uint8_t get_err(long unsigned int *status) {
+        uint8_t errbit0, errbit1, errbit2;
 
-        errcode = (status >> 23) & 7;
-        switch (errcode) {
-                case 0: fcstatus = "No Error";      break;
-                case 1: fcstatus = "ID ERR";        break;
-                case 2: fcstatus = "CMD ERR";       break;
-                case 3: fcstatus = "CRC ERR";       break;
-                case 4: fcstatus = "Preamble ERR";  break;
-                case 5: fcstatus = "Abort ERR";     break;
-                case 6: fcstatus = "Overflow ERR";  break;
-                case 7: fcstatus = "SDM EOF";       break;
+        errbit0 = (*status >> ERRBIT0) & 0x01;
+        errbit1 = (*status >> ERRBIT1) & 0x01;
+        errbit2 = (*status >> ERRBIT2) & 0x01;
+        return ((errbit2 << 2) | (errbit1 << 1) | errbit0) & ERRMASK;
+}
+
+static void print_status_reg(long unsigned int *status)
+{
+        char const *ferr;
+
+        switch (get_err(status)) {
+                case ENOERR: ferr = "No Error";         break;
+                case EID: ferr = "ID ERR";              break;
+                case ECMD: ferr = "CMD ERR";            break;
+                case ECRC: ferr = "CRC ERR";            break;
+                case EPREAM: ferr = "Preamble ERR";     break;
+                case EABRT: ferr = "Abort ERR";         break;
+                case EOVERFL: ferr = "Overflow ERR";    break;
+                case ESDMEOF: ferr = "SDM EOF";         break;
+                default: ferr = "0xDEADBEF0";
         }
-        printf("status: Done=%d, CfgEna=%d, Busy=%d, Fail=%d, FlashCheck=%s\n",
-                ((status >>  8) & 1),
-                ((status >>  9) & 1),
-                ((status >> 12) & 1),
-                ((status >> 13) & 1), fcstatus);
+        printf("machxo2 status: 0x%08lX - done=%d, cfgena=%d, busy=%d, fail=%d,"
+               " devver=%d, err=%s\n",
+                *status, test_bit(DONE, status), test_bit(ENAB, status),
+                test_bit(BUSY, status), test_bit(FAIL, status),
+                test_bit(DVER, status), ferr);
 }
 
 
 void dump_status(int fd) {
-	int 	buf = 0;
+	long unsigned int buf = 0;
 	char reg[4];
 
 	// LSC_READ_STATUS
@@ -89,8 +165,8 @@ void dump_status(int fd) {
 	reg[3] = 0x00;
 	spi_xfer(fd, reg, sizeof(reg), &buf, sizeof(buf));
 	buf = swap_uint32(buf);
-	printf("LSC_READ_STATUS: 0x%08x\n", buf);
-	print_status_reg(buf);
+	printf("LSC_READ_STATUS: 0x%08lx\n", buf);
+	print_status_reg(&buf);
 }
 
 
